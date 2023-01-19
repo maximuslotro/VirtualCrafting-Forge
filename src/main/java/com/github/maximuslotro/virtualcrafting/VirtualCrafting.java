de7109b7d4da9e09c0974f23a015aaa666a922ca
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-01-19 16:44:09 -0600

    Add base code

--- src/main/java/com/github/maximuslotro/virtualcrafting/VirtualCrafting.java
+++ src/main/java/com/github/maximuslotro/virtualcrafting/VirtualCrafting.java
@@ -1,19 +1,21 @@
 package com.github.maximuslotro.virtualcrafting;
 
-import net.minecraft.block.Blocks;
+import net.minecraft.command.CommandSource;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mojang.brigadier.CommandDispatcher;
+
 @Mod(VirtualCrafting.MODID)
 @Mod.EventBusSubscriber(modid = VirtualCrafting.MODID, bus = Bus.MOD,value = Dist.DEDICATED_SERVER)
-public class VirtualCrafting
+public class VirtualCrafting 
 {
     public static final String MODID = "virtualcrafting";
 
@@ -25,10 +27,11 @@ public class VirtualCrafting
     }
 
     @SubscribeEvent
-    private void setup(final FMLCommonSetupEvent event)
+    public void registerCommands(final RegisterCommandsEvent event)
     {
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    	LOGGER.info("Registering Craft Command");
+    	CommandDispatcher<CommandSource> dispatcher = event.getDispatcher();
+		
     }
 
 }
