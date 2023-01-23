a8a29bdda1324b6e935a3c62b55541aa2e172fd5
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-01-23 16:11:53 -0600

    Test- Disable item burning

--- src/main/java/com/github/maximuslotro/virtualcrafting/mixin/VirtualCraftingMixin.java
+++ src/main/java/com/github/maximuslotro/virtualcrafting/mixin/VirtualCraftingMixin.java
@@ -7,19 +7,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 
 @Mixin(ItemEntity.class)
 public class VirtualCraftingMixin {
-	@Inject(at = @At("HEAD"), method = "hurt(Lnet/minecraft/util/DamageSource;F)Z", cancellable = true)
+	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/entity/item/ItemEntity;hurt(Lnet/minecraft/util/DamageSource;F)Z", cancellable = true)
 	private void hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> callback) {
-	  if (source.isFire() && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FIRE_PROTECTION, this.getItem()) > 0) {
-	    callback.setReturnValue(false);
-	  }
+		if(source.isFire()) {
+			callback.setReturnValue(false);
+		}
 	}
 
 	@Shadow
