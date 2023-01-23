711080846a01b887d16c132256bab3b6582f208e
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-01-23 17:23:52 -0600

    Stuff

--- src/main/java/com/github/maximuslotro/virtualcrafting/VirtualCrafting.java
+++ src/main/java/com/github/maximuslotro/virtualcrafting/VirtualCrafting.java
@@ -1,13 +1,20 @@
 package com.github.maximuslotro.virtualcrafting;
 
 import net.minecraft.command.CommandSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,17 +27,21 @@ public class VirtualCrafting
 {
     public static final String MODID = "virtualcrafting";
 
+    private static List<PlayerEntity> crafting;
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
-
+    public static VirtualCrafting instance;
     public VirtualCrafting() {
+    	//Set mod as server only
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         MinecraftForge.EVENT_BUS.register(this);
+        instance = this;
     }
 
     @SubscribeEvent
     public void registerCommands(final RegisterCommandsEvent event)
     {
-    	LOGGER.info("Registering Craft Command");
+    	getLogger().info("Registering Craft Command");
     	CommandDispatcher<CommandSource> dispatcher = event.getDispatcher();
 		
     	CommandCraft command = new CommandCraft("craft", 0, true);
@@ -38,5 +49,26 @@ public class VirtualCrafting
 			dispatcher.register(command.getBuilder());
 		}
     }
+    public void AddCraftingPlayer(PlayerEntity player) {
+    	if (!crafting.contains(player)) {
+    		crafting.add(player);
+    		getLogger().info("THE WORLD IS ENDING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    	}
+    }
+    public void RemoveCraftingPlayer(PlayerEntity player) {
+    	if (crafting.contains(player)) {
+    		crafting.remove(player);
+    	}
+    }
+    public Boolean ContainsCraftingPlayer(PlayerEntity player) {
+    	if (crafting.contains(player)) {
+    		return true;
+    	}
+    	return false;
+    }
+
+	public static Logger getLogger() {
+		return LOGGER;
+	}
 
 }
