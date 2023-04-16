0eb4ba4fd3972e59311a1b0b6e3545c5f04125e7
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2023-04-16 00:04:39 -0500

    Command registration method, potentially fix errors

--- src/main/java/com/github/maximuslotro/virtualcrafting/VirtualCrafting.java
+++ src/main/java/com/github/maximuslotro/virtualcrafting/VirtualCrafting.java
@@ -1,25 +1,18 @@
 package com.github.maximuslotro.virtualcrafting;
 
-import net.minecraft.command.CommandSource;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.github.maximuslotro.virtualcrafting.command.CommandCraft;
-import com.mojang.brigadier.CommandDispatcher;
-
 @Mod(VirtualCrafting.MODID)
-@Mod.EventBusSubscriber(modid = VirtualCrafting.MODID, bus = Bus.MOD,value = Dist.DEDICATED_SERVER)
 public class VirtualCrafting 
 {
     public static final String MODID = "virtualcrafting";
@@ -30,14 +23,14 @@ public class VirtualCrafting
     public VirtualCrafting() {
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
     }
 
     @SubscribeEvent
     public void registerCommands(final RegisterCommandsEvent event)
     {
     	LOGGER.info("Registering Craft Command");
-    	CommandDispatcher<CommandSource> dispatcher = event.getDispatcher();
-    	dispatcher.register(new CommandCraft("craft", 0, true).getBuilder());
+    	CommandCraft.register(event.getDispatcher());
     }
 
 }
